a694fc61aa32e813cae5ded434d63af7fb842978
salvaEbasta <31668481+salvaEbasta@users.noreply.github.com>
2019-03-11 23:48:33 +0100

    Add files via upload

---
+++ versione1/EventBook/src/campi/Field.java
@@ -0,0 +1,52 @@
+package campi;
+
+/**La classe Campo ha il compito di fornire una struttura adatta a contenere una descrizione ed un valore
+ * 
+ * @author Matteo Salvalai [715827], Lorenzo Maestrini[715780], Jacopo Mora [715149]
+ * Il valore che contiene puù essere di diversi tipi, per ogni campo questo deve essere specificato
+ * @param <T> Il tipo di valore che un campo può contenere
+ */
+public class Field <T>{
+	//Attributi
+	/**
+	 * Contiene le informazioni generali di un campo
+	 */
+	private ExpandedHeading heading;
+	/**
+	 * Contiene il valore del campo
+	 */
+	private T value;
+	/**
+	 * La stringa usata per mostrare le principali caratteristiche del campo
+	 */
+	private final static String TOSTRING_FORMAT ="%sValore: %s%n";
+	
+	//Costruttore
+	/**Costruttore del campo, imposta il suo valore a null 
+	 * @param heading La descrizione del campo
+	 */
+	public Field(ExpandedHeading heading) {
+		this.heading = heading;
+		this.value = null;
+	}
+	
+	//Metodi
+	/**Restituisce il nome del campo
+	 * @return Il nome del campo
+	 */
+	public String getName() {
+		return heading.getName();
+	}
+	/**Visualizza le caratteristiche del campo in forma testuale
+	 * @return Le caratteristiche del campo
+	 */
+	public String showFeatures() {
+		return heading.toString();
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString() {
+		return String.format(TOSTRING_FORMAT, heading.toString(), value);
+	}
+}
---
+++ versione1/EventBook/src/categorie/Category.java
@@ -0,0 +1,32 @@
+package categorie;
+
+import campi.FieldSet;
+
+/**Classe con il compito di definire il comportamento di una categoria
+ * 
+ * @author Matteo Salvalai [715827], Lorenzo Maestrini[715780], Jacopo Mora [715149]
+ *
+ */
+public abstract class Category {
+	//Attributi
+	/**
+	 * Contiene un insieme di campi
+	 */
+	private FieldSet commonSet;
+	
+	//Costruttore
+	/**
+	 * Costruttore
+	 */
+	public Category() {
+		commonSet = FieldSet.buildCommonSet();
+	}
+	/**Visualizza la struttura di quello che pu� contenere in forma testuale
+	 * Non visualizza un loro eventuale contenuto
+	 * @return La struttura in forma testuale
+	 */
+	public static String showFeatures(){
+		return FieldSet.showFeatures(FieldSet.buildCommonSet());
+	}
+	
+}
