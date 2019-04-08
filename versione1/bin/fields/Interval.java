55ea42185e1335a05b165b3de6343eb019fd0270
Jacknot <jacknot@x>
2019-04-08 20:52:59 +0200

    Mancava /bin versione 1

---
+++ versione1/bin/fields/Interval.java
@@ -0,0 +1,38 @@
+package fields;
+
+/**Classe con il compito di ospitare due dati numerici interi
+ * @author Matteo Salvalai [715827], Lorenzo Maestrini [715780], Jacopo Mora [715149]
+ *
+ */
+public class Interval {
+
+	//ATTRIBUTI
+	/**
+	 * Il minore fra i due dati
+	 */
+	private int infExt;
+	/**
+	 * Il maggiore fra i due dati
+	 */
+	private int supExt;
+	
+	//COSTRUTTORE
+	/**Costruttore 
+	 * @param infExt Il dato numerico più piccolo si vuole assegnare
+	 * @param supExt Il dato numerico più grande si vuole assegnare
+	 */
+	//estremoSuperiore >= estremoInferiore
+	public Interval(int infExt, int supExt) {
+		this.infExt = infExt;
+		this.supExt = supExt;
+	}
+	//this.estremoSuperiore >= this.estremoInferiore
+
+	//TOSTRING
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString() {
+		return infExt + " - " + supExt;
+	}
+}
